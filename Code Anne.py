f5918a2bbcd1d2654cbd875c057b4378122c9f0a
anne209 <anne.denk@yahoo.de>
2025-08-22 14:57:53 +0200

    set end+start+activity filter

--- Code Anne.py
+++ Code Anne.py
@@ -12,6 +12,7 @@ def import_xes(path_to_xes: str):
 log_sepsis = import_xes('sepsis_case.xes')
 print(log_sepsis)
 
+log_sepsis.to_csv('log_sepsis.csv', index=False)
 
 df_sepsis = log_converter.apply(log_sepsis, variant=log_converter.Variants.TO_DATA_FRAME)
 print(df_sepsis.head()) 
@@ -30,18 +31,37 @@ print(end_activities)
 cases_sepsis = len(log_sepsis['case:concept:name'].unique())
 print(cases_sepsis)
 
-def filter_log(start_acts, log, min_ratio=0.1):
+def filter_log(start_acts, end_acts, log, min_ratio=0.1, end_crit=None, delete_activities=None):
     selected_activities = set()
+    selected_end_acts = set()
+
+
 
     for activity, count in start_acts.items():
         if count / cases_sepsis >= min_ratio:
             selected_activities.add(activity)
     
-    filtered_start = pm4py.filter_start_activities(log, selected_activities)
-    return filtered_start
+    if end_crit is not None:
+        selected_end_acts = set(end_acts.keys()) - set(end_crit)
 
-filter_start = filter_log(start_activities, log_sepsis)
-print(filter_start)
+    filtered_log = pm4py.filter_event_attribute_values(log, "concept:name", delete_activities, level='event', retain=False)
+    filtered_log = pm4py.filter_start_activities(filtered_log, selected_activities)
+    filtered_log = pm4py.filter_end_activities(filtered_log, selected_end_acts)
+
+    return filtered_log
+
+criteria_end=["IV Antibiotics", "ER Sepsis Triage", "Leucocytes", "IV Liquid", "CRP", "LacticAcid", "Admission NC", "ER Triage"]
+deleted_activities = ["Return ER"]
+
+filtered_log= filter_log(start_activities, end_activities, log_sepsis, end_crit=criteria_end, delete_activities=deleted_activities)
+print(filtered_log)
+
+
+start_activities = pm4py.get_start_activities(filtered_log)
+print(start_activities)
+
+end_activities = pm4py.get_end_activities(filtered_log)
+print(end_activities)
 
 dfg, start_activities, end_activities = pm4py.discover_dfg(log_sepsis)
 dfg_output = pm4py.view_dfg(dfg, start_activities, end_activities)
