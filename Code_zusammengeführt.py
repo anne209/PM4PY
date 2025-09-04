b14f0974d95e56ad7b14728e485dbf2c62e1319a
DomKa <bt715009@myubt.de>
2025-09-04 15:52:23 +0200

    comments added

--- Code_zusammengeführt.py
+++ Code_zusammengeführt.py
@@ -8,10 +8,10 @@ import sklearn
 from typing import List, Dict
 import xml.etree.ElementTree as ET
 from pm4py.algo.discovery.temporal_profile import algorithm as temporal_profile_discovery
-from pm4py.algo.discovery.temporal_profile import algorithm as temporal_profile_discovery
+from pm4py.algo.discovery.temporal_profile import algorithm as temporal_profile_discovery # doppelt
 from pm4py.visualization.petri_net import visualizer as pn_visualizer # für Zusatzinfos Performance
 from pm4py.algo.conformance.tokenreplay import algorithm as token_based_replay
-from pm4py.algo.conformance.tokenreplay.diagnostics import duration_diagnostics
+from pm4py.algo.conformance.tokenreplay.diagnostics import duration_diagnostics # doppelt
 from pm4py.algo.conformance.tokenreplay.diagnostics import root_cause_analysis
 from pm4py.visualization.decisiontree import visualizer as dt_vis
 from pm4py.algo.evaluation.generalization import algorithm as generalization_evaluator
